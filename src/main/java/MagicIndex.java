310a15a1b999ad9469fa7293da4325e177747ac2
bmperrien <brandon.perrien@revature.net>
2022-05-10 09:37:22 -0500

    Misread magic index, simplified the solution

--- src/main/java/MagicIndex.java
+++ src/main/java/MagicIndex.java
@@ -7,14 +7,13 @@ public class MagicIndex {
      Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
 
     Follow up: What if the values are not distinct?
-        - If this is the case, just have to go through the whole array to see if there is a magic index
      */
 
 
     //finds the first magic index, could find all by returning a list of integers
     //Returns -1 if there is no magic Index
     public static int magicIndex(int[] arr) {
-        Set<Integer> set = new HashSet<>();
+        /*Set<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             //This checks the set to see if the number has already been seen, if it has it skips to the next value of i
             //This is because if the numbers are distinct, if we have 2 before index 2, we know that index 2 can never
@@ -26,6 +25,16 @@ public class MagicIndex {
                 return i;
             } else {
                 set.add(arr[i]);
+            }
+        }*/
+
+        //Array is sorted, so if i != arr[i] change i to arr[i] because the indeces before that can not be the index
+        //this solution also works for non-distinct values
+        for(int i = 0; i < arr.length; i++){
+            if(i == arr[i]){
+                return i;
+            }else{
+                i = arr[i];
             }
         }
         return -1;
